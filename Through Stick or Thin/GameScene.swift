9712f2bc91ac767b66526a2d8d340e2be92a603d
Snipin74 <josh.cove74@gmail.com>
2018-06-21 16:36:31 -0400

    test

---
+++ Through Stick or Thin/GameScene.swift
@@ -0,0 +1,55 @@
+//
+//  GameScene.swift
+//  Through Stick or Thin
+//
+//  Created by Josh Cove on 2018-06-20.
+//  Copyright © 2018 DaS Kove. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        
+        
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+       
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+     
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
---
+++ Through Stick or ThinTests/Through_Stick_or_ThinTests.swift
@@ -0,0 +1,36 @@
+//
+//  Through_Stick_or_ThinTests.swift
+//  Through Stick or ThinTests
+//
+//  Created by Josh Cove on 2018-06-20.
+//  Copyright © 2018 DaS Kove. All rights reserved.
+//
+
+import XCTest
+@testable import Through_Stick_or_Thin
+
+class Through_Stick_or_ThinTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
